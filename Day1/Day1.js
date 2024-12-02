f15ea40c2a4a8df03eab759ed303eb1643d21481
cesarcuadro <cesarcuadro777@gmail.com>
2024-12-02 13:33:26 -0800

    completed day 1 challenge

--- Day1/Day1.js
+++ Day1/Day1.js
@@ -2,28 +2,49 @@ const fs = require('fs');
 
 const data = fs.readFileSync('/Users/ceasarcuadro/Desktop/AdventOfCode2024/Day1/day1Input.txt', 'utf8');
 // parses all the data as one big string
-console.log(data);
+// console.log(data);
 
-const lines = data.split('\n');
-// splits the data by each line
-// each line turns into a string and is put into an array
-console.log(lines);
-//TODO: figure out why it returns a small number and not a list of all the numbers
-const leftList = lines[0].split('   ').map(Number);
-const rightList = lines[1].split('   ').map(Number);
 
-console.log(leftList)
-console.log(rightList)
+// Parse lines into two lists
+const leftList = [];
+const rightList = [];
+
+// Iterate through each line and split the numbers
+data.trim().split('\n').forEach(line => {
+    const [left, right] = line.trim().split(/\s+/).map(Number);
+    if (!isNaN(left) && !isNaN(right)) {
+        leftList.push(left);
+        rightList.push(right);
+    }
+});
+
+console.log("Left List Length:", leftList.length);
+console.log("Right List Length:", rightList.length);
 
 
 const calculateTotalDistance = (leftList, rightList) => {
-    leftList.sort((a, b) => a - b);
-    rightList.sort((a, b) => a - b);
-    return leftList.reduce((total, left, i) => {
-        return total + Math.abs(left - rightList[i])
-    }, 0)
+    const sortedLeft = [...leftList].sort((a, b) => a - b);
+    const sortedRight = [...rightList].sort((a, b) => a - b);
+    return sortedLeft.reduce((total, left, i) => {
+        return total + Math.abs(left - sortedRight[i]);
+    }, 0);
 };
 
-//console.log(calculateTotalDistance(leftList, rightList));
+const calculateSumOfOccurrences = (leftList, rightList) => {
+    const counts = new Map();
+    rightList.forEach(num => {
+        counts.set(num, (counts.get(num) ?? 0) + 1);
+    });
+
+    return leftList.reduce((total, num) => {
+        return total + num * (counts.get(num) ?? 0);
+    }, 0);
+};
+
+const result = calculateTotalDistance(leftList, rightList);
+console.log('Total Distance: ', result);
+
+const sumOfOccurrences = calculateSumOfOccurrences(leftList, rightList);
+console.log('Part 2: Sum of Occurrences:', sumOfOccurrences);
 
 module.exports = calculateTotalDistance;
