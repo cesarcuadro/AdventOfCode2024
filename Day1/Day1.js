73c5cc25fd4db2a027acaf548eac5ec204eedb56
cesarcuadro <cesarcuadro777@gmail.com>
2024-12-02 12:46:35 -0800

    added gitignore file

--- Day1/Day1.js
+++ Day1/Day1.js
@@ -1,5 +1,20 @@
-const leftList = [3, 4, 2, 1, 3, 3];
-const rightList = [4, 3, 5, 3, 9, 3];
+const fs = require('fs');
+
+const data = fs.readFileSync('/Users/ceasarcuadro/Desktop/AdventOfCode2024/Day1/day1Input.txt', 'utf8');
+// parses all the data as one big string
+console.log(data);
+
+const lines = data.split('\n');
+// splits the data by each line
+// each line turns into a string and is put into an array
+console.log(lines);
+//TODO: figure out why it returns a small number and not a list of all the numbers
+const leftList = lines[0].split('   ').map(Number);
+const rightList = lines[1].split('   ').map(Number);
+
+console.log(leftList)
+console.log(rightList)
+
 
 const calculateTotalDistance = (leftList, rightList) => {
     leftList.sort((a, b) => a - b);
@@ -9,6 +24,6 @@ const calculateTotalDistance = (leftList, rightList) => {
     }, 0)
 };
 
-console.log(calculateTotalDistance(leftList, rightList))
+//console.log(calculateTotalDistance(leftList, rightList));
 
 module.exports = calculateTotalDistance;
